6c190f5d2613a730adbdcbf01c06c931276940b6
Bani Azarael <banimejia@codequark.com>
2019-12-26 11:59:38 -0600

    Add Payment currentTimestamp

--- app/src/main/java/com/jorzet/casmal/models/Payment.kt
+++ app/src/main/java/com/jorzet/casmal/models/Payment.kt
@@ -2,13 +2,14 @@ package com.jorzet.casmal.models
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import java.util.*
 
 @Parcelize
 data class Payment (
     var confirming: Boolean = false,
     var isPremium: Boolean = false,
     var subscription: String,
-    var timeStamp: Int
+    var timeStamp: Long
 ) : Parcelable {
-    constructor() : this(false, false, "", 0)
+    constructor() : this(false, false, "", Date().time)
 }
